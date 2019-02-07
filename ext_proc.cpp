0d4daccf36a16f7619aa802c899f4ec949c7aefe
yukatayu <yukatayu.dev@gmail.com>
2019-02-07 14:35:04 +0900

    add: ext_proc.cpp

---
+++ ext_proc.cpp
@@ -0,0 +1,52 @@
+#include "ext_proc.hpp"
+
+namespace ExtProc{
+	Process::Process(std::string command)
+			: command_ (command)
+			, phase_ (Phase::initialized)
+		{ }
+
+	void Process::run(RunMode_Detach, bool with_stdout){
+		if(phase_ != Phase::initialized)
+			throw std::runtime_error("process has been already launched: " + command_);
+		std::promise<return_type> p;
+		return_data_ = p.get_future();
+		std::thread th(&proc, std::move(p), command_, with_stdout);
+		th.detach();
+	}
+
+	Process::return_type Process::run(RunMode_Await, RunOption_WithStdOut){
+		run(detach, true);
+		auto ret = return_data_.get();
+		phase_ = Phase::finished;
+		return ret;
+	}
+
+	int Process::run(RunMode_Await){
+		int ret = 0;
+		run(detach, false);
+		std::tie(ret, std::ignore) = return_data_.get();
+		phase_ = Phase::finished;
+		return ret;
+	}
+
+	void Process::proc(std::promise<return_type> ret, std::string command, bool get_stdout){
+		std::string output;
+		constexpr int buf_size = 1024;
+		char buf[buf_size + 4];
+		FILE *ep;
+		try{
+			if(!(ep = popen(command.data(), "r")))
+				throw std::runtime_error("could not open: " + command);
+			while(fgets(buf, buf_size, ep))
+				if(get_stdout)
+					output += buf;
+			int return_code = WEXITSTATUS(pclose(ep));
+			ret.set_value({return_code, output});
+		} catch(...) {
+			try {
+				ret.set_exception(std::current_exception());
+			} catch(...) { }
+		}
+	}
+}
